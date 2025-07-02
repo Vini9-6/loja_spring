a2b9b72ce05905239d1bff0d38013dab40959ea5
Vinicius Oliveira <vengcomp@gmail.com>
2025-07-02 12:43:36 -0300

    organiza importações na classe Funcionario e ajusta anotação do salário

--- loja-spring/src/main/java/com/loja/model/Funcionario.java
+++ loja-spring/src/main/java/com/loja/model/Funcionario.java
@@ -1,6 +1,11 @@
 package com.loja.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Funcionarios")
@@ -15,7 +20,7 @@ public class Funcionario {
     @Column(length = 50)
     private String cargo;
 
-    @Column(precision = 10, scale = 2)
+    @Column
     private Double salario;
 
     // Getters e Setters
