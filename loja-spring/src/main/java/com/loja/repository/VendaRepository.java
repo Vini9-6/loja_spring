55722d946e895f13beb4bd2f8aa1a43181a3b235
Vinicius Oliveira <vengcomp@gmail.com>
2025-07-03 10:30:09 -0300

    adiciona método para buscar vendas por período

--- loja-spring/src/main/java/com/loja/repository/VendaRepository.java
+++ loja-spring/src/main/java/com/loja/repository/VendaRepository.java
@@ -1,7 +1,13 @@
 package com.loja.repository;
 
 import com.loja.model.Venda;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VendaRepository extends JpaRepository<Venda, Integer> {
+    @Query("SELECT v FROM Venda v WHERE v.dataVenda BETWEEN :inicio AND :fim")
+    List<Venda> findByPeriodo(@Param("inicio") LocalDateTime inicio, @Param("fim") LocalDateTime fim);
 }
