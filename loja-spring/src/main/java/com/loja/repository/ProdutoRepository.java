25323af2b1741a42cd0dc26517d3e04b46e6b2ec
Vinicius Oliveira <vengcomp@gmail.com>
2025-07-03 10:30:03 -0300

    adiciona método para buscar produtos com estoque baixo

--- loja-spring/src/main/java/com/loja/repository/ProdutoRepository.java
+++ loja-spring/src/main/java/com/loja/repository/ProdutoRepository.java
@@ -1,7 +1,12 @@
 package com.loja.repository;
 
 import com.loja.model.Produto;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+    @Query("SELECT p FROM Produto p WHERE p.quantidadeEstoque < :minimo")
+    List<Produto> findByEstoqueBaixo(@Param("minimo") Integer minimo);
 }
