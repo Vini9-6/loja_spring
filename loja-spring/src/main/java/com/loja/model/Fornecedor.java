5ddcf8feb101a83bd48b1d3d906e0b8a954c2846
Vinicius Oliveira <vengcomp@gmail.com>
2025-07-03 16:02:16 -0300

    corrige anotações das colunas e nome da tabela no modelo Fornecedor

--- loja-spring/src/main/java/com/loja/model/Fornecedor.java
+++ loja-spring/src/main/java/com/loja/model/Fornecedor.java
@@ -2,6 +2,8 @@ package com.loja.model;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,19 +13,20 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "Fornecedores")
+@Table(name = "fornecedores")
 public class Fornecedor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false, length = 100)
+    @Column(name = "nome", nullable = false, length = 100)
     private String nome;
 
-    @Column(length = 100)
+    @Column(name = "contato", length = 100)
     private String contato;
 
     @OneToMany(mappedBy = "fornecedor")
+    @JsonManagedReference
     private List<Produto> produtos;
 
     // Getters e Setters
