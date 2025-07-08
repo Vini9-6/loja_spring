11bf81a2a1fd625c5bf2f48e0a71d67cccb247f6
Vinicius Oliveira <vengcomp@gmail.com>
2025-07-08 17:10:15 -0300

    organiza importações

--- src/main/java/com/loja/repository/ItemVendaRepository.java
+++ src/main/java/com/loja/repository/ItemVendaRepository.java
@@ -1,7 +1,13 @@
 package com.loja.repository;
 
-import com.loja.model.ItemVenda;
+// repositorio de itens de venda
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.loja.model.ItemVenda;
+
 public interface ItemVendaRepository extends JpaRepository<ItemVenda, Integer> {
+    // busca itens de venda por id do produto
+    List<ItemVenda> findByProdutoId(Integer produtoId);
 }
