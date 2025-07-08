ef4acd825973d4c94c48f82faf35535b1603bf31
Vinicius Oliveira <vengcomp@gmail.com>
2025-07-08 17:10:01 -0300

    organiza importações

--- src/main/java/com/loja/repository/ProdutoRepository.java
+++ src/main/java/com/loja/repository/ProdutoRepository.java
@@ -1,12 +1,16 @@
 package com.loja.repository;
 
-import com.loja.model.Produto;
+// repositorio de produto
 import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.loja.model.Produto;
+
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+    // busca produtos com estoque abaixo do minimo
     @Query("SELECT p FROM Produto p WHERE p.quantidadeEstoque < :minimo")
     List<Produto> findByEstoqueBaixo(@Param("minimo") Integer minimo);
 }
